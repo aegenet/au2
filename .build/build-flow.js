5a2e9ce120e92ad6d0ad2d8bf3fdb2f50e32d67f
aegenet <20248096+aegenet@users.noreply.github.com>
2022-01-02 23:30:19 +0100

    chore(build): simplify

--- .build/build-flow.js
+++ .build/build-flow.js
@@ -25,7 +25,14 @@ const tasks = {
   /** Publish */
   publish: project => {
     if (project.publish) {
-      return `cd ./packages/${project.name}/ && npm version "999.${new Date().getTime()}.0" && npm publish --registry=${project.registry || 'https://npm.pkg.github.com/'}`;
+      const cmds = [
+        // Remove devDependencies in npm package
+        `json -I -f ./packages/${project.name}/package.json -e "this.devDependencies={};this.scripts={};this.jest=undefined;"`,
+        `cd ./packages/${project.name}/`,
+        `npm version "999.${new Date().getTime()}.0"`,
+        `npm publish --registry=${project.registry || 'https://npm.pkg.github.com/'}`
+      ];
+      return cmds.join(' && ');
     } else {
       return '';
     }
